0b20eb36a20bb80f8164e428518f1c8d44327b53
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-10-12 09:24:15 +0100

    Added time stamped and uuid model

---
+++ automatilib/i_dot_ai_models.py
@@ -0,0 +1,19 @@
+import uuid
+
+from django.db import models
+
+
+class UUIDPrimaryKeyBase(models.Model):
+    class Meta:
+        abstract = True
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+    modified_at = models.DateTimeField(editable=False, auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["created_at"]
