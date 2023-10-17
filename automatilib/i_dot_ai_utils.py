07d5f7dedaec798d887f3f66ab0241162594898a
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-10-17 11:44:23 +0100

    Added MethodDispatcher to our utils

--- automatilib/i_dot_ai_utils.py
+++ automatilib/i_dot_ai_utils.py
@@ -1,1 +1,13 @@
 """Main module."""
+from django.http import HttpResponseNotAllowed
+
+
+class MethodDispatcher:
+    def __new__(cls, request, *args, **kwargs):
+        view = super().__new__(cls)
+        method_name = request.method.lower()
+        method = getattr(view, method_name, None)
+        if method:
+            return method(request, *args, **kwargs)
+        else:
+            return HttpResponseNotAllowed(request)
