6ca5a60c580d0faf9517775c39bd9ab387ca9f6d
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-10-23 09:57:46 +0100

    Changed COLA login and logout views to be View instead of MethodDispatcher overrides

--- automatilib/cola_auth/i_dot_ai_cola_urls.py
+++ automatilib/cola_auth/i_dot_ai_cola_urls.py
@@ -3,6 +3,6 @@ from i_dot_ai_cola_views import IAIColaLogin, IAIColaLogout
 
 app_name = "cola_auth"
 url_patterns = [
-    path("post-login/", IAIColaLogin, name="post-login"),
-    path("logout/", IAIColaLogout, name="logout"),
+    path("post-login/", IAIColaLogin.as_view(), name="post-login"),
+    path("logout/", IAIColaLogout.as_view(), name="logout"),
 ]
--- automatilib/cola_auth/i_dot_ai_cola_views.py
+++ automatilib/cola_auth/i_dot_ai_cola_views.py
@@ -14,6 +14,7 @@ from django.http import (
 )
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.views import View
 from jose import jwt
 from jose.exceptions import ExpiredSignatureError, JWTClaimsError, JWTError
 
@@ -33,7 +34,7 @@ def get_cola_cognito_user_pool_jwk() -> tuple[dict, str] or None:
     return cola_cognito_user_pool_jwk, cola_issuer
 
 
-class IAIColaLogout(MethodDispatcher):
+class IAIColaLogout(View):
     @abstractmethod
     def post_logout(self):
         """
@@ -61,7 +62,7 @@ class IAIColaLogout(MethodDispatcher):
         return response
 
 
-class IAIColaLogin(MethodDispatcher):
+class IAIColaLogin(View):
     @abstractmethod
     def pre_login(self):
         """
