9037099e8a7d5abc85cf46a9619335ef6e546897
george <g.e.c.burton@gmal.com>
2023-10-30 07:44:19 +0000

    changed README to .md

--- cola/urls.py
+++ cola/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from cola import views
 
 url_patterns = [
-    path("post-login/", views.ColaLogin.as_view(), name="post-login"),
+    path("login/", views.ColaLogin.as_view(), name="login"),
     path("logout/", views.ColaLogout.as_view(), name="logout"),
 ]
--- example_project/urls.py
+++ example_project/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-import cola
+from cola.urls import url_patterns as cola_urls
 from example_project import views
 
 urlpatterns = [
     path("hello/", views.hello_world, name="hello-world"),
     path("my_account/", views.my_account, name="my-account"),
-    path("login/", cola.ColaLogin.as_view(), name="login"),
-    path("logout/", cola.ColaLogout.as_view(), name="logout"),
-]
+] + cola_urls
