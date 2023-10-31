a6176efd20c5ed6b9d390f53c7920de49b84da40
george <g.e.c.burton@gmal.com>
2023-10-31 11:25:26 +0000

    reduce diff

--- automatilib/core/settings.py
+++ automatilib/core/settings.py
@@ -1,11 +1,11 @@
-# INSTALLED_APPS = [
-#     "automatilib.automatilib.core",
-#     "automatilib.automatilib.cola",
-#     "django_use_email_as_username.apps.DjangoUseEmailAsUsernameConfig",
-# ]
+INSTALLED_APPS = [
+    "automatilib.core",
+    "automatilib.cola",
+    "django_use_email_as_username.apps.DjangoUseEmailAsUsernameConfig",
+]
 
-# AUTHENTICATION_BACKENDS = [
-#     "automatlib.cola.backend.AuthenticationBackend",
-# ]
-#
-# AUTH_USER_MODEL = "automatilib.core.User"
+AUTHENTICATION_BACKENDS = [
+    "automatlib.cola.backend.AuthenticationBackend",
+]
+
+AUTH_USER_MODEL = "automatilib.core.User"
