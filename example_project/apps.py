f50a8d227bd687352f4958183caea42670438097
George Burton <8233643+gecBurton@users.noreply.github.com>
2023-11-01 09:25:24 +0000

    Update example_project/apps.py

--- example_project/apps.py
+++ example_project/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ExampleProjectConfig(AppConfig):
     name = "example_project"
     label = "example_project"
-    verbose_name = "i.AI's COLA Authentication"
+    verbose_name = "example application using COLA Authentication"
