fd354a1d0648ce05e1d347fa5ae634744e674a30
George Burton <8233643+gecBurton@users.noreply.github.com>
2023-11-02 09:24:48 +0000

    Delete automatilib/core/settings.py

--- automatilib/core/settings.py
+++
@@ -1,9 +0,0 @@
-INSTALLED_APPS = [
-    "automatilib.core",
-    "automatilib.cola",
-    "django_use_email_as_username.apps.DjangoUseEmailAsUsernameConfig",
-]
-
-AUTHENTICATION_BACKENDS = [
-    "automatlib.cola.backend.AuthenticationBackend",
-]
