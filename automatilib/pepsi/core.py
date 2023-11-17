3d674be29810ca7deedecd12b66d56c412e1f8e8
george <g.e.c.burton@gmal.com>
2023-11-17 12:33:03 +0000

    wip

--- automatilib/pepsi/core.py
+++ automatilib/pepsi/core.py
@@ -1,41 +1,72 @@
 from base64 import urlsafe_b64encode
 from datetime import datetime, timedelta
 
-import pytest
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from jose import jwt
 
 
-from django.http import HttpRequest, HttpResponse
-
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-
-public_key = private_key.public_key()
-public_numbers = public_key.public_numbers()
-e, n = public_numbers.e, public_numbers.n
 
 
 def url_encode_public_numbers(number):
-    # Serialize "e" (public exponent) and "n" (modulus) to bytes
+    """Serialize "e" (public exponent) and "n" (modulus) to bytes"""
     number_as_bytes = number.to_bytes((number.bit_length() + 7) // 8, "big")
     return urlsafe_b64encode(number_as_bytes).decode("utf-8")
 
 
-# Encode "e" and "n" as base64 URL-safe strings
-e_base64 = url_encode_public_numbers(e)
-n_base64 = url_encode_public_numbers(n)
+class FakeTokenFactory:
+    def __init__(self, iss, aud):
+        self.iss = iss
+        self.aud = aud
 
-# Create a JSON payload
-payload = {"alg": "RS256", "e": e_base64, "kid": "MY-KID-ID", "kty": "RSA", "n": n_base64, "use": "sig"}
-cola_cognito_user_pool_jwk = {"keys": [payload]}
+        self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
-private_pem = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption(),
-)
+    def cola_cognito_user_pool_jwk(self) -> dict:
+        # Create a JSON payload
+        public_key = self.private_key.public_key()
+        public_numbers = public_key.public_numbers()
+        e, n = public_numbers.e, public_numbers.n
+
+        # Encode "e" and "n" as base64 URL-safe strings
+        e_base64 = url_encode_public_numbers(e)
+        n_base64 = url_encode_public_numbers(n)
+
+        payload = {"alg": "RS256", "e": e_base64, "kid": "MY-KID-ID", "kty": "RSA", "n": n_base64, "use": "sig"}
+        return {"keys": [payload]}
+
+    def generate_token(self, email: str, **kwargs: str) -> str:
+        payload = {
+            "sub": "2612f274-f081-70fc-e3f3-7da285b561a4",
+            "email_verified": True,
+            "custom:lastLogin": "2023-10-06T11:06:13.730Z",
+            "custom:features": "test",
+            "iss": self.iss,
+            "aud": self.aud,
+            "event_id": "09102e2f-f60f-480f-99e0-09b4ea451568",
+            "token_use": "id",
+            "custom:phoneNumber": "+07700900676",
+            "auth_time": 1696591062,
+            "exp": datetime.utcnow() + timedelta(hours=1),
+            "iat": 1696591062,
+            "email": email,
+        }
+
+        payload.update(kwargs)
+
+        pem = self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+
+        token = jwt.encode(
+            payload,
+            pem.decode(),
+            algorithm="RS256",
+            headers={"alg": "RS256", "kid": "MY-KID-ID"},
+        )
+        return token
 
 
-def login(request: HttpRequest) ->HttpResponse:
+
 
