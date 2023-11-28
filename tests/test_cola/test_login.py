59a06fca8d14e74c39d788bad8b98b86347add28
George Burton <8233643+gecBurton@users.noreply.github.com>
2023-11-28 09:42:31 +0000

    Bugfix/handle non cola authentication attempt (#15)

--- tests/test_cola/test_login.py
+++ tests/test_cola/test_login.py
@@ -89,3 +89,26 @@ def test_invalid_token(client, jwt_payload, private_pem, cola_cognito_user_pool_
         response = client.get(reverse("hello-world"), follow=True)
 
     assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_authenticate_fails(cola_client, cola_cognito_user_pool_jwk):
+    """same as test_login but this time we force the authenticate method to fail"""
+    response = cola_client.get(reverse("hello-world"))
+    assert response.status_code == 302
+
+    # now we mock requests to the outside world
+    with patch("requests.get") as mock_get, patch("automatilib.cola.views.authenticate") as mock_authenticator:
+        mock_response = mock_get.return_value
+        mock_response.status_code = 200
+        mock_response.json.return_value = cola_cognito_user_pool_jwk
+
+        mock_authenticator.return_value = None
+
+        response = cola_client.get(reverse("hello-world"), follow=True)
+
+    assert response.status_code == 401
+    assert response.content.decode() == "Unauthorized"
+
+    # finally we check that only the right url was mocked
+    mock_get.assert_called_once_with(COLA_JWK_URL, timeout=5)
